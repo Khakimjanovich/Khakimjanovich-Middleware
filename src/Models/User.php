8c523e3792708b912fe3778ea017b85fa5b24ed9
khkaimjanovich <yunusalikhakimjanovich@gmail.com>
2021-05-28 20:03:33 +0500

    Update User.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,9 +2,19 @@
 
 namespace INBRAIN\OMP\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Http;
 
-class User extends Model
+class User extends Authenticatable
 {
     protected $guarded = [];
+
+    protected $appends = [
+        'credentials'
+    ];
+
+    public function getCredentialsAttribute(): array
+    {
+        return Http::withHeaders(['Authorization' => 'Bearer ' . request()->bearerToken()])->get(config('inbrain.auth_server') . '/v1/me')->json();
+    }
 }
